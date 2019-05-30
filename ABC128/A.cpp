8e64a064d31c7cd3efc51846400ea1d1e1c60b20
zeronosu77108 <taiga77108@gmail.com>
2019-05-30 23:59:29 +0900

    Add A

---
+++ ABC128/A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    int a,p;
+    cin >> a >> p;
+    cout << (a*3+p)/2 << endl;
+}
