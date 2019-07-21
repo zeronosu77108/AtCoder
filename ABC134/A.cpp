edce96ee52a2aadd9dcc14e97afe8a0c756d14a0
zeronosu77108 <taiga77108@gmail.com>
2019-07-22 08:15:29 +0900

    Add A

---
+++ ABC134/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int r;
+  cin >> r;
+  cout << 3*r*r << endl;
+}
