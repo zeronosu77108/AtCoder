886866760ba769f8298c91c84ad1d49bdd58c998
zeronosu77108 <taiga77108@gmail.com>
2019-08-19 08:22:27 +0900

    Add A in ABC138

---
+++ ABC138/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int a;
+  string s;
+  cin >> a >> s;
+
+  cout << (a>=3200? s : "red") << endl;
+}
