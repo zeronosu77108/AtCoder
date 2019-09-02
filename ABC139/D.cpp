a9450770fe84fd1fe2ce893f4a543dc47b47ac05
zeronosu77108 <taiga77108@gmail.com>
2019-09-02 13:34:14 +0900

    Add D in ABC139

---
+++ ABC139/D.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << (long long)n*(n-1)/2 << endl;
+}
+
+/*
+1 2 3 4 5 6 7 8 9 10 11 12 13
+*/
