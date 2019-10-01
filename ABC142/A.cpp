dfd953ccee28462dafab002f4d76973d8e52680e
zeronosu77108 <taiga77108@gmail.com>
2019-10-01 13:28:07 +0900

    Add A in ABC142

---
+++ ABC142/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << (n-(n/2))/(double)n << endl;
+}
