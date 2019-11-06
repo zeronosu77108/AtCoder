f1935f7cde05e9ab2f574a154a0ba1e45f2b4b47
zeronosu77108 <taiga77108@gmail.com>
2019-11-06 18:11:18 +0900

    Add A in ABC001

---
+++ ABC001/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int h1,h2;
+    cin >> h1 >> h2;
+    cout << h1 - h2 << endl;
+}
