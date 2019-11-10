367779ac553c072f08e6544473af5f4b634e9d33
zeronosu77108 <taiga77108@gmail.com>
2019-11-10 13:30:15 +0900

    Add A in nikkei

---
+++ nikkei2019-2-qual/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << (n-1)/2 << endl;
+}
