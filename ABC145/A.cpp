fda0dd48579046a97c311e2f9430ec6a22849194
zeronosu77108 <taiga77108@gmail.com>
2019-11-19 15:16:36 +0900

    Add A in ABC145

---
+++ ABC145/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+    int r;
+    cin >> r;
+
+    cout << r*r << endl;
+}
