a5e9ebe0cfee9dfb3186172efb83250baae619bb
zeronosu77108 <taiga77108@gmail.com>
2019-12-22 23:32:22 +0900

    Add A in ABC148

---
+++ ABC148/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+
+typedef long long int64;
+
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << 6 - a - b << endl;
+}
