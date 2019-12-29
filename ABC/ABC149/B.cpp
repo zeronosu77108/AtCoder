47d4ff4d1971695d65f54e706f8d93fbd10e4470
zeronosu77108 <taiga77108@gmail.com>
2019-12-29 23:45:19 +0900

    Add B in ABC149

---
+++ ABC/ABC149/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+
+typedef long long int64;
+
+
+int main() {
+    int64 a,b,k;
+    cin >> a >> b >> k;
+    cout << max(0LL, a - k) << " ";
+    cout << max(0LL, b - max(0LL, k-a)) << endl;
+}
