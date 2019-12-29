7c1b767f94debf8cae64e2abbbb9b0ce267d3d0d
zeronosu77108 <taiga77108@gmail.com>
2019-12-29 16:56:42 +0900

    Add A in ABC140

---
+++ ABC140/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << pow(n,3) << endl; 
+}
