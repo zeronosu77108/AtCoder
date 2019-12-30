407e42e87d0ced0cb60c6cae27e6d33685e3b6f7
zeronosu77108 <taiga77108@gmail.com>
2019-12-30 15:26:05 +0900

    Add A in ABC006

---
+++ ABC/ABC006/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+
+typedef long long int64;
+
+
+int main() {
+    int n;
+    cin >> n;
+    cout << (n%3? "NO" : "YES") << endl;
+}
