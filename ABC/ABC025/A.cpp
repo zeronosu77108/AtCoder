c0dc2e5940f6813bbbbe80b4ee70d88b24c70df0
zeronosu77108 <taiga77108@gmail.com>
2020-01-03 22:09:36 +0900

    Add A in ABC025

---
+++ ABC/ABC025/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+
+typedef long long int64;
+
+
+int main() {
+    string s;
+    int n;
+    cin >> s >> n;
+
+    cout << s[(n-1)/5] << s[(n-1)%5] << endl;
+}
