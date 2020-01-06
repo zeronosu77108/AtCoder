5e2637c3e4e66956603cef730d653c008af52a81
zeronosu77108 <taiga77108@gmail.com>
2020-01-06 21:49:05 +0900

    Add B in ABC034

---
+++ ABC/ABC034/B.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+
+typedef long long int64;
+
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 2 == 0) n--;
+    else n++;
+
+    cout << n << endl;
+}
