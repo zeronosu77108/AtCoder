1abe8e34d1c81646601f9f2c7c3c7889441ebbab
zeronosu77108 <taiga77108@gmail.com>
2020-01-08 21:29:27 +0900

    Add A in ABC037

---
+++ ABC/ABC037/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+
+typedef long long int64;
+
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if (a > b) swap(a,b);
+
+    cout << c / a << endl;
+}
