ecedf03f1340b1a8706900162e9235dd3de66365
zeronosu77108 <taiga77108@gmail.com>
2020-01-31 00:25:51 +0900

    Add A in ABC047

---
+++ ABC/ABC047/A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    vector<int> candy(3);
+    for (int i=0; i<3; i++) cin >> candy[i];
+    sort(candy.begin(), candy.end());
+
+    bool ans = candy[0] + candy[1] == candy[2];
+    cout << (ans ? "Yes" : "No") << endl;
+}
