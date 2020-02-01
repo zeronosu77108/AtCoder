63de384a986c8838d252d5a8c8ccc8c15c67f3ad
zeronosu77108 <taiga77108@gmail.com>
2020-02-01 18:52:06 +0900

    Add B in ABC048

---
+++ ABC/ABC048/B.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int64 a,b,x;
+    cin >> a >> b >> x;
+
+    int64 bdiv = b/x + 1;
+    int64 adiv = (a==0? 0 : (a-1)/x+1);
+    cout << bdiv - adiv << endl;
+}
