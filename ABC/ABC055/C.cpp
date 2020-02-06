b007c57689c65059cc81543173a8b3f71f2f435b
zeronosu77108 <taiga77108@gmail.com>
2020-02-06 22:07:20 +0900

    Add C in ABC055

---
+++ ABC/ABC055/C.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int64 n,m;
+    int64 ans = 0;
+    cin >> n >> m;
+    ans += min(n, m/2);
+    m = max(0LL, m - ans * 2);
+    ans += m / 4;
+
+    cout << ans << endl;
+}
