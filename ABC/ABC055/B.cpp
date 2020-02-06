a8e708962c0e2c30f772e231004da8e0e86e8f8c
zeronosu77108 <taiga77108@gmail.com>
2020-02-06 22:07:20 +0900

    Add B in ABC055

---
+++ ABC/ABC055/B.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+const int MOD = 1000000007;
+int main() {
+    int n;
+    int64 ans = 1;
+    cin >> n;
+    for (int64 i=1; i<=n; i++) ans = (ans%MOD) * i;
+
+    cout << ans % MOD << endl;
+}
