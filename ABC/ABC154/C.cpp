3e80ca5e5d629217d1bd667c892a3d14e4a1d623
zeronosu77108 <taiga77108@gmail.com>
2020-02-09 23:54:36 +0900

    Add C in ABC154

---
+++ ABC/ABC154/C.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i=0; i<n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
+
+    cout << (n == a.size()? "YES" : "NO") << endl;
+}
