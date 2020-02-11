9969723b83eac5cd5fcd15a63bbce1b9601e5693
zeronosu77108 <taiga77108@gmail.com>
2020-02-11 16:12:19 +0900

    Add B in ABC059

---
+++ ABC/ABC059/B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int a,b,c;
+    bool ans = false;
+    cin >> a >> b >> c;
+
+    for (int i=1; i<=b; i++) {
+        if (a*i % b == c) ans = true;
+    }
+
+    cout << (ans? "YES" : "NO") << endl;
+}
