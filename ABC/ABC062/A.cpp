90230155281e8ba0df25849ad7dcb688a22487b2
zeronosu77108 <taiga77108@gmail.com>
2020-02-16 19:24:26 +0900

    Add A in ABC062

---
+++ ABC/ABC062/A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    vector<int> group = {0,1,2,1,3,1,3,1,1,3,1,3,1};
+    int x,y;
+    cin >> x >> y;
+
+    cout << (group[x] == group[y]? "Yes" : "No") << endl;
+}
