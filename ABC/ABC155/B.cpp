6e42175d75d4fda2f1b0c95d1e5bed46614563dc
zeronosu77108 <taiga77108@gmail.com>
2020-02-17 08:33:23 +0900

    Add B in ABC155

---
+++ ABC/ABC155/B.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool ans = true;
+
+    for (int i=0; i<n; i++) {
+        int a; cin >> a;
+        if (a % 2 != 0) continue;
+        if (a % 3 != 0 && a % 5 != 0) ans = false;
+    }
+
+    cout << (ans? "APPROVED" : "DENIED") << endl;
+}
