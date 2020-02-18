24fed33ad973e901698d45630a2ca3bdc1830e90
zeronosu77108 <taiga77108@gmail.com>
2020-02-18 22:25:16 +0900

    Add C in ABC068

---
+++ ABC/ABC068/C.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> g(n+1);
+    for (int i=0; i<m; i++) {
+        int a,b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+
+    bool ans = false;
+    for (auto u : g[1]) {
+        for (auto v : g[u]) {
+            if (v == n) ans = true;
+        }
+    }
+    cout << (ans? "POSSIBLE" : "IMPOSSIBLE") << endl;
+}
