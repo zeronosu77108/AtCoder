53ef2acf78991e8c724d92f5e6e817de6bea59d4
zeronosu77108 <taiga77108@gmail.com>
2020-02-20 01:31:45 +0900

    Add C in ABC095

---
+++ ABC/ABC095/C.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int a,b,c,x,y;
+    cin >> a >> b >> c >> x >> y;
+    int ans = a*x + b*y;
+    ans = min(ans, c*x*2 + b*max(0,(y-x)));
+    ans = min(ans, c*y*2 + a*max(0,(x-y)));
+
+    cout << ans << endl;
+}
