1b94d1aeabe1d00ca08ea817b7da3b1e41c03bd4
zeronosu77108 <taiga77108@gmail.com>
2020-03-16 17:05:57 +0900

    Add B in ABC073

---
+++ ABC/ABC073/B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i=0; i<n; i++) {
+        int l,r;
+        cin >> l >> r;
+        ans += r - l + 1;
+    }
+    cout << ans << endl;
+}
