cd78f527414d4bcf9b761830e89fc7f1575c5471
zeronosu77108 <taiga77108@gmail.com>
2020-03-18 19:56:23 +0900

    Add A in ABC075

---
+++ ABC/ABC075/A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    vector<int> abc(3);
+    for (int i=0; i<3; i++) cin >> abc[i];
+    sort(abc.begin(), abc.end());
+    int ans = 0;
+    if (abc[0] == abc[1]) ans = abc[2];
+    if (abc[2] == abc[1]) ans = abc[0];
+
+    cout << ans << endl;
+}
