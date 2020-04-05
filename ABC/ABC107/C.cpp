6e5945809bd57ee996077eb2d689c4d6ce181070
zeronosu77108 <taiga77108@gmail.com>
2020-04-05 13:31:53 +0900

    Add C in ABC107

--- ABC/ABC107/C.cpp
+++ ABC/ABC107/C.cpp
@@ -1,33 +1,19 @@
 #include <iostream>
-#include <iomanip>
 #include <vector>
-#include <utility>
-#include <map>
-#include <algorithm>
-#include <queue>
-#include <cmath>
-#include <numeric>
 
 using namespace std;
-struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
-
-typedef long long int64;
-
 
 int main() {
-    int64 n,k;
-    int64 ans = INT_MAX;
+    int n,k;
     cin >> n >> k;
-    vector<int64> x(n);
+    vector<int> a(n);
+    for (int i=0; i<n; i++) cin >> a[i];
 
-    for(int64 i=0; i<n; i++) cin>>x[i];
-
-    for(int64 i=0; i+k<=n; i++) {
-        int64 r = i + k - 1;
-        ans = min(ans, abs(x[i]) + abs(x[i] - x[r]));
-        ans = min(ans, abs(x[r]) + abs(x[r] - x[i]));
+    int ans = INT_MAX;
+    for (int i=0; i+k-1<n; i++) {
+        ans = min(ans, abs(a[i]) + abs(a[i+k-1] - a[i]));
+        ans = min(ans, abs(a[i+k-1]) + abs(a[i] - a[i+k-1]));
     }
 
-    if (n == 1) ans = abs(x[0]);
     cout << ans << endl;
 }
