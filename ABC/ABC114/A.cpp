5ff7ee160c7f36ee7bc1a7d89ac064292fe01d16
zeronosu77108 <taiga77108@gmail.com>
2020-04-08 23:41:21 +0900

    Add A in ABC114

---
+++ ABC/ABC114/A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x==3 || x==5 || x==7) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
