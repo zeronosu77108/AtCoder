b53528eff57675a65227cf5373e8162d82abe37d
zeronosu77108 <taiga77108@gmail.com>
2020-08-02 23:03:57 +0900

    Add B in ABC174

---
+++ ABC/ABC174/B.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n,d;
+    cin >> n >> d;
+
+    int ans = 0;
+    for (int i=0; i<n; i++) {
+        int x,y;
+        cin >> x >> y;
+        if (hypot(x,y) <= d) ans++;
+    }
+    cout<<ans<<endl;
+}
