a76c2fa5dce13396d05d161b1a13a7e98f45c84c
zeronosu77108 <taiga77108@gmail.com>
2020-08-02 23:03:59 +0900

    Add D in ABC174

---
+++ ABC/ABC174/D.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    map<char,int> mp;
+    for (int i=0; i<n; i++) mp[s[i]]++;
+
+    int cnt = count(s.begin(), s.begin() + mp['R'], 'R');
+    cout << mp['R'] - cnt << endl;
+}
