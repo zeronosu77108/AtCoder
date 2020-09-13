48b3ca896514406d3de5199bb6790788f17b3ba7
zeronosu77108 <taiga77108@gmail.com>
2020-09-13 23:04:40 +0900

    Add B in ABC178

---
+++ ABC/ABC178/B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int64 a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    int64 ans = max({a*c, a*d, b*c, b*d});
+    cout << ans << endl;
+}
