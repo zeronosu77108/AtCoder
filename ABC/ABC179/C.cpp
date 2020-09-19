7193851c08b40aa61c89b3e1ebc4d1dab230578d
zeronosu77108 <taiga77108@gmail.com>
2020-09-19 23:43:28 +0900

    Add C in ABC179

---
+++ ABC/ABC179/C.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int64 ans = 0;
+    for (int64 a=1; a<=n; a++) {
+        for (int64 b=1; a*b<=n; b++) {
+            int64 c = n - a*b;
+            if (0 < c) ans++;
+        }
+    }
+
+    cout << ans << endl;
+}
