980f2842851a2604c26991278342f1ed084d1c6b
zeronosu77108 <taiga77108@gmail.com>
2020-10-05 20:52:42 +0900

    Add M in dp

---
+++ others/dp/M.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <unordered_map>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+const int MOD = 1000000007;
+class mint {
+    using int64 = long long;
+private:
+    int64 x;
+public:
+    mint(const int64 x1=0) { x = x1%MOD; };
+
+    mint& operator++(int n) { x+=1; return *this; };
+    mint& operator--(int n) { x-=1; return *this; };
+    mint& operator=(int64 n) { x=n%MOD; return *this; };
+    mint& operator--() {x-=1; return *this; };
+    mint& operator+=(const mint a) { if ( (x+=a.x) >= MOD) x-=MOD; return *this; }
+    mint& operator-=(const mint a) { if ( (x+= MOD-a.x) >= MOD) x-= MOD; return *this; }
+    mint& operator*=(const mint a) { (x*=a.x) %= MOD; return *this; }
+    mint& operator/=(const mint a) { return (*this) *= a.inv();}
+    mint operator+(const mint a) const { mint res(*this); return res+=a; }
+    mint operator-(const mint a) const { mint res(*this); return res-=a; }
+    mint operator*(const mint a) const { mint res(*this); return res*=a; }
+    mint operator/(const mint a) const { mint res(*this); return res/=a; }
+    mint inv() const { return pow(MOD-2);}
+    friend ostream& operator<<(ostream &os, const mint a) noexcept { return os << a.x; }
+    constexpr bool operator == (const mint& r) const noexcept { return this->x == r.x; }
+    constexpr bool operator != (const mint& r) const noexcept { return this->x != r.x; }
+    mint pow(long long t) const {
+        if (!t) return mint(1);
+        mint a = pow(t>>1);
+        a*=a;
+        if(t&1) a*= *this;
+        return a;
+    }
+};
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector a(n, 0);
+    for (int i=0; i<n; i++) cin >> a[i];
+
+    vector dp(n+1, vector<mint>(k+1, 0));
+    vector<mint> pre(k+2, 1);
+    dp[0][0] = 1;
+    pre[0] = 0;
+
+    for (int i=0; i<n; i++) {
+        vector<mint> csum(k+2, 0);
+        for (int j=0; j<=k; j++) {
+            dp[i+1][j] += pre[j+1] - pre[max(0, j - a[i])];
+            csum[j+1] += csum[j] + dp[i+1][j];
+        }
+        swap(pre, csum);
+    }
+
+    cout << dp[n][k] << endl;
+}
