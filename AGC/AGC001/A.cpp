af8d5c2e6b575dc96438f1ba2e7d9d088a3715f7
zeronosu77108 <taiga77108@gmail.com>
2020-10-28 00:57:15 +0900

    Add A in AGC001

---
+++ AGC/AGC001/A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    cin >> n;
+    vector l(2*n, 0);
+    for (int i=0; i<2*n; i++) cin >> l[i];
+    sort(l.begin(), l.end());
+
+    int ans = 0;
+    for (int i=0; i<2*n-1; i+=2) {
+        ans += min(l[i], l[i+1]);
+    }
+
+    cout << ans << endl;
+}
