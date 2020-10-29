8bdda2e735ed030e41beeb12c6a34b29d72c11db
zeronosu77108 <taiga77108@gmail.com>
2020-10-29 16:36:17 +0900

    Add A in AGC025

---
+++ AGC/AGC025/A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<v<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    {
+        int t = n;
+        while(t>0) {
+            ans += t%10;
+            t/=10;
+        }
+    }
+
+    if (ans==1 && n!=1) ans = 10;
+    cout << ans << endl;
+}
