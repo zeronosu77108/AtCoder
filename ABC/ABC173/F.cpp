b5754f679c1f4c3f1a2299be89346152e30335b2
zeronosu77108 <taiga77108@gmail.com>
2020-12-30 23:05:32 +0900

    Add F in ABC173

---
+++ ABC/ABC173/F.cpp
@@ -0,0 +1,28 @@
+//
+// Created by zeronosu77108 on Dec 29, 2020.
+//
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+using P = pair<int, int>;
+
+int main() {
+    long n;
+    cin >> n;
+
+    long ans = 0;
+    for (long i=1; i<=n; i++) ans += i*(i+1)/2;
+
+    // 全組合せから u-v が採用される範囲分引く
+    // → 1,2,3,u と v,v+1,...,n までの範囲は u-v が採用される
+    for (int i=0; i<n-1; i++) {
+        long u, v;
+        cin >> u >> v;
+        if (u > v) swap(u, v);
+        ans -= (u * (n-v+1));
+    }
+
+    cout << ans << endl;
+}
