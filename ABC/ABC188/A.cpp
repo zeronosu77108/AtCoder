bce0124e2a2ee2ed49b8371b41c5176fd36e24d2
zeronosu77108 <taiga77108@gmail.com>
2021-01-11 15:49:03 +0900

    Add A in ABC188

---
+++ ABC/ABC188/A.cpp
@@ -0,0 +1,35 @@
+//
+// Created by zeronosu77108 on Jan 10, 2021.
+//
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <complex>
+#include <optional>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<(v)<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (x < y) swap(x,y);
+
+    if (x < y+3) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No"<< endl;
+    }
+}
