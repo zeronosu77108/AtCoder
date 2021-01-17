c2743d65e23f0bb6e634138cb47f22b4b489f218
zeronosu77108 <taiga77108@gmail.com>
2021-01-17 15:17:05 +0900

    Add A in keyence2021

---
+++ others/keyence2021/A.cpp
@@ -0,0 +1,43 @@
+//
+// Created by zeronosu77108 on Jan 16, 2021.
+//
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <complex>
+#include <optional>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<(v)<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector a(n, 0L);
+    for (int i=0; i<n; i++) cin >> a[i];
+
+    vector b(n, 0L);
+    for (int i=0; i<n; i++) cin >> b[i];
+
+    long ci = 0;
+    long amax = 0;
+    for (int i=0; i<n; i++) {
+        amax = max(amax, a[i]);
+        ci = max(ci, amax*b[i]);
+
+        cout << ci << endl;
+    }
+}
