81d59ed88928e5b646ba84bc18ad0697ba769151
zeronosu77108 <taiga77108@gmail.com>
2021-01-24 22:24:23 +0900

    Add A in ABC189

---
+++ ABC/ABC189/A.cpp
@@ -0,0 +1,34 @@
+//
+// Created by zeronosu77108 on Jan 23, 2021.
+//
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <complex>
+#include <optional>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<(v)<<endl;}
+
+using int64 = long long;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (3 == count(s.begin(), s.end(), s.front())) {
+        cout << "Won" << endl;
+    } else {
+        cout << "Lost" << endl;
+    }
+}
