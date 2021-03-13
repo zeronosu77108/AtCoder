a5804e0b3401d9a06eb2f18548652aa7592f038b
zeronosu77108 <taiga77108@gmail.com>
2021-03-13 15:20:25 +0900

    Add C in ABC192

---
+++ ABC/ABC192/C.cpp
@@ -0,0 +1,41 @@
+//
+// Created by zeronosu77108 on Mar 13, 2021.
+//
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <complex>
+#include <optional>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<(v)<<endl;}
+
+using int64 = long long;
+
+int main() {
+    string s;
+    int k;
+    cin >> s >> k;
+
+    for (int i=0; i<k; i++) {
+        sort(s.rbegin(), s.rend());
+        int g1 = stoi(s);
+
+        sort(s.begin(), s.end());
+        int g2 = stoi(s);
+
+        s = to_string(g1 - g2);
+    }
+
+    cout << s << endl;
+}
