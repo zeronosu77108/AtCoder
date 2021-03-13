a47a4272beca17b44ffb285de26ed4e178292c7f
zeronosu77108 <taiga77108@gmail.com>
2021-03-13 13:34:00 +0900

    Add B in ABC193

---
+++ ABC/ABC193/B.cpp
@@ -0,0 +1,38 @@
+//
+// Created by zeronosu77108 on Mar 13, 2021.
+//
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <complex>
+#include <optional>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<(v)<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> ans;
+    for (int i=0; i<n; i++) {
+        int a, p, x;
+        cin >> a >> p >> x;
+        if (a < x) ans.emplace_back(p);
+    }
+
+
+    cout << (ans.size()>0? *min_element(ans.begin(), ans.end()) : -1) << endl;
+}
