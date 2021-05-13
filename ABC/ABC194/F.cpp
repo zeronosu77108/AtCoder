e2a85f8e3853b39bfbd54a76366766c65df5b68e
zeronosu77108 <taiga77108@gmail.com>
2021-05-13 22:22:00 +0900

    Add F in ABC194

---
+++ ABC/ABC194/F.cpp
@@ -0,0 +1,69 @@
+//
+// Created by zeronosu77108 on Mar 13, 2021.
+//
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <complex>
+#include <optional>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const optional<T>&v){o<<(v? to_string(v.value()) : "-");return o;}
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<(v)<<endl;}
+
+using int64 = long long;
+
+const int MOD = 1'000'000'007;
+class mint {
+    using int64 = long long;
+private:
+    int64 x;
+public:
+    mint(const int64 x1=0) { x = x1%MOD; };
+
+    mint& operator++(int n) { x+=1; return *this; };
+    mint& operator--(int n) { x-=1; return *this; };
+    mint& operator=(int64 n) { x=n%MOD; return *this; };
+    mint& operator--() {x-=1; return *this; };
+    mint& operator+=(const mint a) { if ( (x+=a.x) >= MOD) x-=MOD; return *this; }
+    mint& operator-=(const mint a) { if ( (x+= MOD-a.x) >= MOD) x-= MOD; return *this; }
+    mint& operator*=(const mint a) { (x*=a.x) %= MOD; return *this; }
+    mint& operator/=(const mint a) { return (*this) *= a.inv();}
+    mint operator+(const mint a) const { mint res(*this); return res+=a; }
+    mint operator-(const mint a) const { mint res(*this); return res-=a; }
+    mint operator*(const mint a) const { mint res(*this); return res*=a; }
+    mint operator/(const mint a) const { mint res(*this); return res/=a; }
+    mint inv() const { return pow(MOD-2);}
+    friend ostream& operator<<(ostream &os, const mint a) noexcept { return os << a.x; }
+    constexpr bool operator == (const mint& r) const noexcept { return this->x == r.x; }
+    constexpr bool operator != (const mint& r) const noexcept { return this->x != r.x; }
+    mint pow(long long t) const {
+        if (!t) return mint(1);
+        mint a = pow(t>>1);
+        a*=a;
+        if(t&1) a*= *this;
+        return a;
+    }
+};
+
+int main() {
+    string _s;
+    int k;
+    cin >> _s >> k;
+    int n = _s.size();
+
+    vector s(n, 0);
+    for (int i=0; i<n; i++) s[i] = stoi(string(1, _s[i]), nullptr, 16);
+
+    vector dp()
+}
