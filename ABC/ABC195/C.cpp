848fcaab8f7a276f23e000e17820696048caf091
zeronosu77108 <taiga77108@gmail.com>
2021-05-13 22:23:05 +0900

    Add C in ABC195

---
+++ ABC/ABC195/C.cpp
@@ -0,0 +1,38 @@
+//
+// Created by zeronosu77108 on Mar 13, 2021.
+//
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <complex>
+#include <optional>
+
+using namespace std;
+struct aaa{aaa(){cin.tie(nullptr); ios::sync_with_stdio(false); cout<<fixed<<setprecision(20);};}aaa;
+template <class T>ostream &operator<<(ostream &o,const optional<T>&v){o<<(v? to_string(v.value()) : "-");return o;}
+template <class T>ostream &operator<<(ostream &o,const vector<T>&v){o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
+#define debug(v) {cerr<<"\033[1;36m[debug]\033[m "<<#v<<" : "<<(v)<<endl;}
+
+using int64 = long long;
+
+int main() {
+    int64 n;
+    cin >> n;
+
+    int64 ans = 0;
+    int64 s = 1000;
+    while (s <= n) {
+        ans += n - s + 1;
+        s *= 1000;
+    }
+
+    cout << ans << endl;
+}
